1689f8d9003d5a4e3359bb2e7504eb2b15fc67d5
erginbostanci <43159350+erginbostanci@users.noreply.github.com>
2019-03-02 19:58:47 -0600

    Update main.py

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,61 +1,56 @@
-"""This script will review election results, tally the votes, and identify the winner"""
 
 import os
 import csv
 
-filename = input("Please enter the filename for the data you would like to review: ")
-total_votes = 0
-candidate = ""
-candidate_votes = {}
-candidate_percentages ={}
-winner_votes = 0
-winner = ""
+file_db = input("Enter the filename you want to analyze ")
+votes = 0
+candidate_db_ = ""
+candidate_db__votes = {}
+candidate_db__percent ={}
+win_vt = 0
+win_db = ""
 
-# open csv file
-filepath = os.path.join("raw_data", filename)
-with open(filepath,'r', newline="") as csvfile:
+file_db_path = os.path.join("raw_data", file_db)
+with open(file_db_path,'r', newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     next(csvreader)
 
-    # tally votes
     for row in csvreader:
-        total_votes = total_votes + 1
-        candidate = row[2]
-        if candidate in candidate_votes:
-            candidate_votes[candidate] = candidate_votes[candidate] + 1
+        votes = votes + 1
+        candidate_db_ = row[2]
+        if candidate_db_ in candidate_db__votes:
+            candidate_db__votes[candidate_db_] = candidate_db__votes[candidate_db_] + 1
         else:
-            candidate_votes[candidate] = 1
+            candidate_db__votes[candidate_db_] = 1
+            
+for person, vote_count in candidate_db__votes.items():
+    candidate_db__percent[person] = '{0:.0%}'.format(vote_count / votes)
+    if vote_count > win_vt:
+        win_vt = vote_count
+        win_db = person
 
-# calculate vote percentage and identify winner
-for person, vote_count in candidate_votes.items():
-    candidate_percentages[person] = '{0:.0%}'.format(vote_count / total_votes)
-    if vote_count > winner_votes:
-        winner_votes = vote_count
-        winner = person
-
-dashbreak = "-------------------------"
+leave_space = "-------------------------"
 
 # print out results
 print("Election Results")
-print(dashbreak)
-print(f"Total Votes: {total_votes}")
-print(dashbreak)
-for person, vote_count in candidate_votes.items():
-    print(f"{person}: {candidate_percentages[person]} ({vote_count})")
-print(dashbreak)
-print(f"Winner: {winner}")
-print(dashbreak)
+print(leave_space)
+print(f"Total Votes: {votes}")
+print(leave_space)
+for person, vote_count in candidate_db__votes.items():
+    print(f"{person}: {candidate_db__percent[person]} ({vote_count})")
+print(leave_space)
+print(f"win_db: {win_db}")
+print(leave_space)
 
-# save summary to txt
-save_file = filename.strip(".csv") + "_results.txt"
-filepath = os.path.join(".", save_file)
-with open(filepath,'w') as text:
-    text.write(dashbreak + "\n")
-    text.write(f"Total Votes: {total_votes}" + "\n")
-    text.write(dashbreak + "\n")
-    for person, vote_count in candidate_votes.items():
-        text.write(f"{person}: {candidate_percentages[person]} ({vote_count})" + "\n")
-    text.write(dashbreak + "\n")
-    text.write(f"Winner: {winner}" + "\n")
-    text.write(dashbreak + "\n")
+store_results_as_txt = file_db.strip(".csv") + "_results.txt"
+file_db_path = os.path.join(".", store_results_as_txt)
+with open(file_db_path,'w') as text:
+    text.write(leave_space + "\n")
+    text.write(f"Total Votes: {votes}" + "\n")
+    text.write(leave_space + "\n")
+    for person, vote_count in candidate_db__votes.items():
+        text.write(f"{person}: {candidate_db__percent[person]} ({vote_count})" + "\n")
+    text.write(leave_space + "\n")
+    text.write(f"win_db: {win_db}" + "\n")
+    text.write(leave_space + "\n")
